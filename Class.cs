2612f671a78f6ee6b0e2e8da97b3eaf7bddddf3a
Erza0838 <muhammaderza884@gmail.com>
2023-06-01 06:42:49 +0700

    Belajar inheritance hari ke 2

---
+++ Class.cs
@@ -0,0 +1,25 @@
+﻿public class InfoPerson
+{
+    public string InheritePersonName = "";
+    public string InheritePersonCareer = "";
+    public int InheritePersonAge = 0;
+
+    public void ShowInforPerson(string InheritePersonCareer)
+    {
+      Console.WriteLine("Nama saya adalah " + InheritePersonName);
+      Console.WriteLine("Umur saya " + InheritePersonAge);
+      Console.WriteLine("Saya disini sebagai " + InheritePersonCareer);
+      Console.WriteLine("");
+    }
+}
+
+public class inforPerson2 : InfoPerson
+{
+    public void ShowInforPerson2(string InheritePersonCareer2)
+    {
+      Console.WriteLine("Nama saya adalah " + InheritePersonName);
+      Console.WriteLine("Umur saya " + InheritePersonAge);
+      Console.WriteLine("Saya disini sebagai " + InheritePersonCareer2);
+      Console.WriteLine("");
+    }
+}
