653fc7de01a937d1f1a28ff89e3d7113467fbfec
Erza0838 <muhammaderza884@gmail.com>
2023-06-01 06:44:22 +0700

    Belajar inheritance hari ke-2

--- Inherrite.cs
+++ Inherrite.cs
@@ -1,13 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using system;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HelloWorld
 {
   class Program
-  {
-    static void Main(string[] args)
-    {
-      Console.WriteLine("Hello World!");    
+  { 
+    static void Main(string[] arg)
+    {   
+      string[] namePerson = { "Muhammad erza", "Ryanto" };
+      string[] personCareer = { "Programer", "Desainer grafis" };
+      int[] AgePerson = { 19, 20 };
+
+      // Buat object dan jalankan funciton untuk menampilkan data pada field
+      InfoPerson PersonData = new InfoPerson();
+      PersonData.InheritePersonName = namePerson[0]; 
+      PersonData.InheritePersonAge = AgePerson[0];
+      PersonData.ShowInforPerson(personCareer[0]);
+      
+      PersonData.InheritePersonName = namePerson[1]; 
+      PersonData.InheritePersonAge = AgePerson[1];
+      PersonData.ShowInforPerson(personCareer[1]);
+
     }
   }
 }
